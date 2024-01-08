35e1b477abf816985de33cd65e234dabfe79615d
Labibuzzaman Labib <mdlabib315@gmail.com>
2024-01-08 09:07:12 +0600

    upadted

--- lib/presentation/ui/screens/HomeScreen.dart
+++ lib/presentation/ui/screens/HomeScreen.dart
@@ -1,6 +1,9 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:crafty_bay/presentation/ui/utility/appcolors.dart';
 import 'package:crafty_bay/presentation/ui/utility/assets_path.dart';
 import 'package:flutter/material.dart';
 import '../widgets/home/circle_Icon_Button.dart';
+import '../widgets/home/imageCarousel.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -13,33 +16,75 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        elevation: 0,
-        actions: [
-          CircleIconButton(
-            onTap: () {},
-            iconData: Icons.person,
+      appBar: appBar,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            children: [
+              buildTextFormField,
+              const SizedBox(height: 10),
+              Home_imageCarousel()
+            ],
           ),
-          const SizedBox(
-            width: 8,
-          ),
-          CircleIconButton(
-            onTap: () {},
-            iconData: Icons.call,
-          ),
-          const SizedBox(
-            width: 8,
-          ),
-          CircleIconButton(
-            onTap: () {},
-            iconData: Icons.notifications_active,
-          ),
-        ],
-        title: Hero(
-          tag: 'appLogo',
-          child: Image.asset(AssetsPath.navLogo),
         ),
+      ),
+    );
+  }
+
+  TextFormField get buildTextFormField {
+    return TextFormField(
+      decoration: InputDecoration(
+        hintText: 'Search',
+        filled: true,
+        fillColor: Colors.grey.shade200,
+        prefixIcon: Icon(
+          Icons.search,
+          color: Colors.grey,
+        ),
+        border: OutlineInputBorder(
+          borderSide: BorderSide.none,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide.none,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide.none,
+          borderRadius: BorderRadius.circular(8),
+        ),
+      ),
+    );
+  }
+
+  AppBar get appBar {
+    return AppBar(
+      backgroundColor: Colors.white,
+      elevation: 0,
+      actions: [
+        CircleIconButton(
+          onTap: () {},
+          iconData: Icons.person,
+        ),
+        const SizedBox(
+          width: 8,
+        ),
+        CircleIconButton(
+          onTap: () {},
+          iconData: Icons.call,
+        ),
+        const SizedBox(
+          width: 8,
+        ),
+        CircleIconButton(
+          onTap: () {},
+          iconData: Icons.notifications_active,
+        ),
+      ],
+      title: Hero(
+        tag: 'appLogo',
+        child: Image.asset(AssetsPath.navLogo),
       ),
     );
   }
---
+++ lib/presentation/ui/widgets/home/imageCarousel.dart
@@ -0,0 +1,83 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+
+import '../../utility/appcolors.dart';
+
+class Home_imageCarousel extends StatefulWidget {
+  const Home_imageCarousel({
+    super.key,
+    this.height,
+  });
+
+  final double? height;
+
+  @override
+  State<Home_imageCarousel> createState() => _Home_imageCarouselState();
+}
+
+class _Home_imageCarouselState extends State<Home_imageCarousel> {
+  ValueNotifier<int> _currentIndex = ValueNotifier(0);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CarouselSlider(
+          options: CarouselOptions(
+            height: widget.height ?? 200.0,
+            onPageChanged: (index, reason) {
+              _currentIndex.value = index;
+            },
+            viewportFraction: 1,
+            autoPlay: true,
+          ),
+          items: [1, 2, 3, 4, 5].map((i) {
+            return Builder(
+              builder: (BuildContext context) {
+                return Container(
+                  width: MediaQuery.of(context).size.width,
+                  margin: const EdgeInsets.symmetric(horizontal: 3.0),
+                  decoration: BoxDecoration(
+                    color: AppColors.primaryColor,
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  alignment: Alignment.center,
+                  child: Text(
+                    'text $i',
+                    style: const TextStyle(fontSize: 16.0),
+                  ),
+                );
+              },
+            );
+          }).toList(),
+        ),
+        const SizedBox(height: 4),
+        ValueListenableBuilder(
+            valueListenable: _currentIndex,
+            builder: (context, index, _) {
+              return Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  for (int i = 0; i < 7; i++)
+                    Container(
+                      height: 14,
+                      width: 14,
+                      margin: const EdgeInsets.all(2),
+                      decoration: BoxDecoration(
+                          color: i == index
+                              ? AppColors.primaryColor
+                              : Colors.white,
+                          border: Border.all(
+                            color: i == index
+                                ? AppColors.primaryColor
+                                : Colors.grey,
+                          ),
+                          borderRadius: BorderRadius.circular(30)),
+                    )
+                ],
+              );
+            })
+      ],
+    );
+  }
+}
